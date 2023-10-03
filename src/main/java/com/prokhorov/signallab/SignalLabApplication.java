f2958a98b5a1bb151d1809e18410a70279ce0883
Константин Резников (KeenEye) <reznikov@keeneye.pro>
2023-10-03 13:02:23 +0400

    delete unused code

--- src/main/java/com/prokhorov/signallab/SignalLabApplication.java
+++ src/main/java/com/prokhorov/signallab/SignalLabApplication.java
@@ -1,16 +1,9 @@
 package com.prokhorov.signallab;
 
-import java.util.Scanner;
-
 public class SignalLabApplication {
-	private final static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		Console.start();
-		/*
-		SoundHandler handler = new SoundHandler();
-		handler.play(1);
-		handler.play(8);*/
 	}
 
 }
